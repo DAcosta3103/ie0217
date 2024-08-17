c86f854ad66c5dc05af77bb6483694d71df23215
DAcosta3103 <117118915+DAcosta3103@users.noreply.github.com>
2024-08-16 18:48:09 -0600

    Delete Sesiones/Sesion1 directory
    Se borra el folder de sesiones para empezar desde cero

--- Sesiones/Sesion1/README.md
+++
@@ -1,2 +0,0 @@
-# ie0217
-Este repositorio será utilizado para el curso IE0217 - Estructuras Abstractas de Datos y Algoritmos para Ingeniería
--- Sesiones/Sesion1/adiosMundo.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main () {
-    std::cout << "Adios mundo, estamos haciendo nuestro trabajo en el curso IE0217!";
-    return 0;
-}
