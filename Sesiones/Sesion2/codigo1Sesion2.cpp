6743c297e02d8afbcc756ced1febfb828ba1fb77
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-08-21 03:02:48 -0600

    Se agrega el código utilizado para probar la función como condicional del operador '?:'
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

---
+++ Sesiones/Sesion2/codigo1Sesion2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Hola mundo!\n";
+
+    int var1 = 0;
+    int var2;
+
+    var2 = (var1 == 3) ? 15 : 27;
+
+    std::cout << "var2 : \n" << var2 << "";
+
+    return 0;
+}
