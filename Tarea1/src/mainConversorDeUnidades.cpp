d250a20705cb87d48d978be4155c96d1dcc9a4b1
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-08-29 08:51:01 -0600

    Se termina la documentacion doxygen del ejercicio 1, con licencia MIT
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

--- Tarea1/src/mainConversorDeUnidades.cpp
+++ Tarea1/src/mainConversorDeUnidades.cpp
@@ -1,3 +1,29 @@
+/**
+ * @file mainConversorDeUnidades.cpp
+ * @brief ARchivo main del script para el conversor de unidades de longitud, peso y temperatura.
+ * 
+ * MIT License
+
+ * Copyright (c) 2024 DAcosta3103
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "conversorDeUnidades.hpp"
 
 #include <iostream>
@@ -5,7 +31,14 @@
 
 using namespace std;
 
+/**
+ * @brief La funcion principal del programa. 
+ * Llama a la funcion menu y cae dentro de la misma funcion menu
+ */
+
 int main() {
-    menuPrincipal();        // Se llama la función menú
+    menuPrincipal();        
     return 0;
 }
+
+
