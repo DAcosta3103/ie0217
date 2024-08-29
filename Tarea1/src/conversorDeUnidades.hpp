d5db310dfdd72d0d7bcf86311a391f4c40485748
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-08-29 08:23:12 -0600

    Se añade la documentacón estilo doxygen al headerfile
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

--- Tarea1/src/conversorDeUnidades.hpp
+++ Tarea1/src/conversorDeUnidades.hpp
@@ -4,43 +4,90 @@
 #include <string>
 using namespace std;
 
-// Enumeraciones para las opciones de unidades
+
+/**
+ * Enumeración y estructura para las opciones de unidades de longitud.
+ */
 enum struct OpcionesLongitud {
-    metro = 1,
-    kilometro,
-    milimetro,
-    centimetro,
-    milla,
-    pie,
-    pulgada
+    metro = 1, /**< OpcionesLongitud value metro. */  
+    kilometro, /**< OpcionesLongitud value kilometro. */
+    milimetro, /**< OpcionesLongitud value milimetro. */  
+    centimetro, /**< OpcionesLongitud value centimetro. */
+    milla, /**< OpcionesLongitud value milla. */
+    pie, /**< OpcionesLongitud value pie. */
+    pulgada /**< OpcionesLongitud value pulgada. */
 };
+
+
+/**
+ * Enumeración y estructura para las opciones de unidades de peso
+ */
 
 enum struct OpcionesPeso {
-    gramo = 1,
-    kilogramo,
-    miligramo,
-    libra,
-    tonelada,
-    onza
+    gramo = 1, /**< OpcionesPeso value gramo. */  
+    kilogramo, /**< OpcionesPeso value kilogramo. */
+    miligramo, /**< OpcionesPeso value miligramo. */
+    libra, /**< OpcionesPeso value libra. */
+    tonelada, /**< OpcionesPeso value tonelada. */
+    onza /**< OpcionesPeso value onza. */
 };
+
+
+/**
+ * @brief Enumeración y estructura para las opciones de unidades de temperatura 
+ */
 
 enum struct OpcionesTemperatura {
-    celsius = 1,
-    fahrenheit,
-    kelvin
+    celsius = 1, /**< OpcionesTemperatura value celsius. */
+    fahrenheit, /**< OpcionesTemperatura value fahrenheit. */
+    kelvin /**< OpcionesTemperatura value kelvin. */
 };
 
-// Funciones para convertir opciones a strings
+
+/**
+ * @brief Función para convertir opciones de longitud a su respectivo string 
+ * @param op La opción de longitud de tipo OpcionesLongitud.
+ * @return El nombre de la unidad de longitud en string.
+ */
 string unidadString(OpcionesLongitud op); 
+
+/**
+ * @brief Función para convertir opciones de peso a su respectivo string 
+ * @param op La opción de peso de tipo OpcionesPeso.
+ * @return El nombre de la unidad de peso en string.
+ */
 string unidadString(OpcionesPeso op); 
+
+/**
+ * @brief Función para convertir opciones de temperatura a su respectivo string 
+ * @param op La opción de temperatura de tipo OpcionesTemperatura.
+ * @return El nombre de la unidad de temperatura en string.
+ */
 string unidadString(OpcionesTemperatura op); 
 
-// Funciones para ejecutar las conversiones
+
+/**
+ * @brief Función para calcular conversiones de longitud 
+ * @return void
+ */
 void convertirUnidadesLongitud(); 
+
+/**
+ * @brief Función para calcular conversiones de peso
+ * @return void
+ */
 void convertirUnidadesPeso(); 
+
+/**
+ * @brief Función para calcular conversiones de temperatura
+ * @return void
+ */
 void convertirUnidadesTemperatura();
 
-// Función para mostrar el menú principal
+/**
+ * @brief Función para mostrar el menú principal
+ * @return void
+ */
 void menuPrincipal();
 
 #endif // CONVERSOR_DE_UNIDADES_H_
