988c17261464cf08c36c3aa36fe509f33d795277
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-09-14 15:41:44 -0600

    Se añade la función de inicialización en un árbol predefinido. También se añaden las licencias referentes para doxygen
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

--- Tarea3/src/arbol/arbol.hpp
+++ Tarea3/src/arbol/arbol.hpp
@@ -1,3 +1,30 @@
+/**
+ * @file arbol.hpp
+ * @brief Headerfile para la clase ArbolBinario
+ * 
+ * MIT License
+
+ * Copyright (c) 2024 DAcosta3103
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef ARBOL_HPP
 #define ARBOL_HPP
 
@@ -25,6 +52,8 @@ class ArbolBinario {
          * @param valor El valor del nuevo nodo
          */
         void insertarNodo(NodoArbol*& nodo, int valor);
+
+
 
         /**
          * @brief Método para obtener el valor mínimo del árbol
@@ -81,6 +110,12 @@ class ArbolBinario {
          * @param valor El valor del nodo a insertar
          */
         void insertar(int valor);
+
+        /**
+         * @brief Método para inicializar el árbol en valores predefinidos
+         * 
+         */
+        void inicializar();
 
         /**
          * @brief Método público para llamar a la función de eliminar nodo
