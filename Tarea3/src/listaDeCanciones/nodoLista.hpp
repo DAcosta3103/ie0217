685400e94fea27f544d028ff099b6571c8941c82
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-09-14 13:43:55 -0600

    Se modifica la clase Nodo para acomodarse al problema con canciones
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

--- Tarea3/src/listaDeCanciones/nodoLista.hpp
+++ Tarea3/src/listaDeCanciones/nodoLista.hpp
@@ -1,19 +1,20 @@
 #ifndef NODO_LISTA_HPP
 #define NODO_LISTA_HPP
+#include "cancion.hpp"
 
 /**
  * @brief Estructura Nodo encargada del funcionamiento de los nodos en la lista enlazada
  * 
  */
-struct Nodo {
-
-    int dato;                   // Se crea el atributo dato
-    Nodo* siguiente;            // Se crea un puntero que señala a la estructura Nodo, y se le llama siguiente
+class Nodo {
+    public:
+        Cancion* cancion;                   // Se crea el atributo dato
+        Nodo* siguiente;            // Se crea un puntero que señala a la estructura Nodo, y se le llama siguiente
     
     
     // Función constructora de la estructura Nodo
     // Recibe un valor y le asigna ese valor al atributo 'dato', mientras que el puntero 'siguiente' apunta a NULL
-    Nodo(int valor) : dato(valor), siguiente(nullptr) {}
+    Nodo(const Cancion& cancion) : cancion(cancion), siguiente(nullptr) {}
 };
 
 #endif
