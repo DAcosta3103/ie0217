df1854ca386bd7cf99082e1f36c5468e2eaa16ad
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-09-14 13:44:56 -0600

    Se termina de planear la clase en el headerfile de Lista Enlazada
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

---
+++ Tarea3/src/listaDeCanciones/lista.hpp
@@ -0,0 +1,83 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+#include "nodoLista.hpp"
+#include <iostream>
+
+/**
+ * @brief Clase principal que maneja la Lista Enlazada
+ * 
+ */
+class ListaEnlazada {
+    private:
+        Nodo* cabeza;
+    
+    public:
+        /**
+         * @brief Construtor para la clase Lista Enlazada
+         * 
+         */
+        ListaEnlazada();
+
+        /**
+         * @brief Destructor para la clase Lista Enlazada
+         * 
+         */
+        ~ListaEnlazada();
+
+        /**
+         * @brief Método para inicializar la lista de canciones con tres canciones predefinidas
+         * 
+         */
+        void inicializar();
+        
+        /**
+         * @brief Método para insertar una canción al inicio de la lista
+         * 
+         * @param cancion Cancion por insertar 
+         */
+        void insertarInicio(Cancion& cancion);
+
+        /**
+         * @brief Método para insertar una canción al final de la lista
+         * 
+         * @param cancion Canción por insertar
+         */
+        void insertarFinal(Cancion& cancion);
+
+        /**
+         * @brief Método para sertar una canción en una posición en específico de la lista
+         * 
+         * @param cancion Cancion por insertar
+         */
+        void insertarPosicion(Cancion& cancion);
+
+        /**
+         * @brief Método para eliminar una canción
+         * 
+         * @param nombre Canción por eliminar
+         */
+        void eliminarCancion(string& nombre);
+
+        /**
+         * @brief Método para modificar una canción
+         * 
+         * @param nombre nombre de la canción por modificar
+         * @param nuevaCancion El nuevo objeto canción con los datos nuevos
+         */
+        void modificarCancion(string& nombre, Cancion& nuevaCancion);
+
+        /**
+         * @brief Método para imprimir la lista de canciones
+         * 
+         */
+        void imprimirLista() const;
+
+
+};
+
+
+
+
+
+#endif
