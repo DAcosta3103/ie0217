d5c011029927eeffe1dd39008586553794858282
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-11-10 03:21:00 -0600

    se crea la función main
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

---
+++ Tarea6/src/administracion/main.cpp
@@ -0,0 +1,28 @@
+#include "sistema.hpp"
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int num_clientes = 5;
+    int num_operadores = 3;
+    vector<thread> clientes, operadores;      // se crean vectores de hilos para manejar los clientes y operadores
+
+    for (int i = 0; i < num_clientes; ++i)
+        clientes.emplace_back(cliente, i + 1);
+
+    for (int i = 0; i < num_operadores; ++i)
+        operadores.emplace_back(operador, i + 1);
+
+    thread metricas_thread(mostrar_metricas);   // se crea un hilo que emplea la función mostrar_metricas
+
+    // se itera sobre cada hilo en ambos vectores y llama a join() en ellos 
+    for (auto& c : clientes) c.join(); 
+    for (auto& o : operadores) o.join();    
+
+    // se bloquea la ejecución del hilo principal hasta que el hilo metricas_thread termine
+    metricas_thread.join();
+
+    return 0;
+}
